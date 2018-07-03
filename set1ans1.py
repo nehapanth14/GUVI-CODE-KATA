898973fd956eb3076e4f76391ab45fd4ad024553
Pravinbat <33679801+Pravinbat@users.noreply.github.com>
2018-07-03 19:48:38 +0530

    Update set1ans1.py

--- set1ans1.py
+++ set1ans1.py
@@ -8,13 +8,13 @@ def c(num):
     else:
        print("Negative number")
 
-num = float(input("Enter the number between 1 to 1000 : "))
+num = float(input("Enter the number between 1 to 1000 : ")) # Getting the input from user
 
 
-if num >1000:
+if num >1000:      #Checking whether the user has entered the number within 1000
     print "please enter the number within 1000"
 else:
-    c(num)
+    c(num) 
      
 
   
