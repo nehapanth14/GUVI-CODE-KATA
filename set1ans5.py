87a6a43fece01d4216890dad2439c28beb444d73
Pravinbat <33679801+Pravinbat@users.noreply.github.com>
2018-07-03 21:55:07 +0530

    Update set1ans5.py

--- set1ans5.py
+++ set1ans5.py
@@ -1,8 +1,15 @@
 print "Enter three numbers"
 
-num1 = input("Enter the number 1 : ")
-num2 = input("Enter the number 2 : ")
-num3 = input("Enter the number 3 : ")
+
+try :
+    num1 = int(input("Enter the number 1 : "))
+    num2 = int(input("Enter the number 2 : "))
+    num3 = int(input("Enter the number 3 : "))
+except Exception:
+    print ""
+    print "       Invalid Input      "
+
+    
 
 if (num1 > num2 and num1 > num3):
     print ("{0} is the greatest number ".format(num1))
