125b309fdf05f758c23592ca110e5a79e25e64e4
Pravinbat <33679801+Pravinbat@users.noreply.github.com>
2018-07-03 20:43:38 +0530

    Add files via upload

---
+++ setans2.py
@@ -0,0 +1,19 @@
+def get(num):
+    if (num % 2) == 0:
+       print("{0} is Even".format(num))
+    elif num > 0 or num != 0:
+       print("{0} is Odd".format(num))
+
+
+
+try :
+    num = int(input("Enter the number between 1 to 100000 : "))
+except Exception:
+    print ""
+    print "       Invalid Input      "
+
+if num <=100000:
+    get(num)
+else:
+    print "Enter the number below 100000"
+
